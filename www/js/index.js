9670f031cbc2e56e40d4b0d5d808cb34b152d32a
FranitaTriandhani <48430884+FranitaTriandhani@users.noreply.github.com>
2019-04-08 13:12:33 +0700

    Update index.js
    revisi 1

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=PNAxtc5OcPxpsVqwqdJ3JXRHQgX041w0GSJdy5Rj";
         
         $("#loadImage").click(function(){
             $.ajax({
